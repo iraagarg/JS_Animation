c1b26a3d55407ac7290e81ae37b9a84517d66f0e
Iraa Garg <iragarg07@gmail.com>
2025-09-20 11:54:13 +0530

    resize canvas with window

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById("rainCanvas");
 const ctx =  canvas.getContext("2d");
 
-
+function resizeCanvas() {
+   canvas.width = window.innerWidth;
+  canvas.height  = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
